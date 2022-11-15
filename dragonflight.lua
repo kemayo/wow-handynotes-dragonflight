5e4ed84bf9ec8316410104afaef8432dcdb53258
David Lynch <kemayo@gmail.com>
2022-11-14 22:20:01 -0600

    Expedition Scout's Pack, hidden by default
    Locations are mostly from wowhead

--- dragonflight.lua
+++ dragonflight.lua
@@ -1,11 +1,5 @@
 local myname, ns = ...
 
-ns.hiddenConfig = {
-    groupsHidden = true,
-}
-
-ns.groups["junk"] = BAG_FILTER_JUNK
-
 ns.WAKINGSHORES = 2022
 ns.OHNAHRANPLAINS = 2023
 ns.AZURESPAN = 2024
@@ -15,5 +9,20 @@ ns.FORBIDDENREACH = 2026
 ns.FACTION_DRAGONSCALE = 2507
 
 ns.MAXLEVEL = {ns.conditions.QuestComplete(67030), ns.conditions.Level(70)}
+
+ns.hiddenConfig = {
+    groupsHidden = true,
+}
+
+ns.defaults.profile.groupsHiddenByZone[ns.WAKINGSHORES] = {
+    scoutpack = true,
+}
+ns.defaults.profile.groupsHiddenByZone[ns.OHNAHRANPLAINS] = {
+    scoutpack = true,
+}
+
+ns.groups["junk"] = BAG_FILTER_JUNK
+ns.groups["scoutpack"] = "Expedition Scout's Pack"
+
 
 -- Rescued Waddles: 70872
