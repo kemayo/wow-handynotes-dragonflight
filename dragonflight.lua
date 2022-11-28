4d33af38063fa8ac2c5d1adcebae0cc650a040cc
David Lynch <kemayo@gmail.com>
2022-11-28 11:03:05 -0600

    Notes about achievements to pursue

--- dragonflight.lua
+++ dragonflight.lua
@@ -38,3 +38,8 @@ ns.groups["dailymount"] = "Daily Mounts"
 -- Rescued Waddles: 70872
 
 -- unlocked dragon customization: 68797
+
+-- TODO achievements:
+-- Selfie achievements: Framing A New Perspective (16634), That's Pretty Neat! (16446)
+-- Fragments of History (16323)
+-- Honor our Ancestors (16423)
