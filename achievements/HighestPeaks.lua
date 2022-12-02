d826c36840a286ccadaf5cac583efd2da166aa8d
David Lynch <kemayo@gmail.com>
2022-12-02 00:08:05 -0600

    Add the questids to Highest Peaks
    Fixes #4

--- achievements/HighestPeaks.lua
+++ achievements/HighestPeaks.lua
@@ -2,6 +2,7 @@ local myname, ns = ...
 
 local HIGHEST = {
 	achievement=15890,
+	achievementNotFound=true,
 	minimap=false, -- there's a flag once they're unlocked
 	texture=ns.atlas_texture("racing", {r=0, g=1, b=0}),
 	requires=ns.DRAGONRIDING,
@@ -12,33 +13,33 @@ local HIGHEST = {
 }
 
 ns.RegisterPoints(ns.WAKINGSHORES, {
-	[23835308] = {},
-	[43976294] = {},
-	[54797412] = {},
-	[56534513] = {},
-	[73353884] = {},
+	[28714773] = {quest=70826}, -- was 23835308?
+	[43976294] = {quest=70825},
+	[54797412] = {quest=71204},
+	[56534513] = {quest=70823},
+	[73353884] = {quest=70824},
 }, HIGHEST)
 
 ns.RegisterPoints(ns.OHNAHRANPLAINS, {
-	[28317764] = {},
-	[30393646] = {},
-	[57753080] = {},
-	[86313928] = {},
+	[28317764] = {quest=71200},
+	[30393646] = {quest=71207},
+	[57753080] = {quest=70827},
+	[86313928] = {quest=71208},
 }, HIGHEST)
 
 ns.RegisterPoints(ns.AZURESPAN, {
-	[31912703] = {},
-	[37466620] = {},
-	[46142498] = {},
-	[63084867] = {},
-	[71986162] = {},
-	[77431837] = {},
+	[31912703] = {quest=71215},
+	[37466620] = {quest=71216},
+	[46142498] = {quest=71218},
+	[63084867] = {quest=71220},
+	[74854324] = {quest=71221}, -- was 71986162?
+	[77431837] = {quest=71217},
 }, HIGHEST)
 
 ns.RegisterPoints(ns.THALDRASZUS, {
-	[34048484] = {},
-	[46107397] = {},
-	[50168163] = {},
-	[65727498] = {},
-	[66195378] = {},
+	[34048484] = {quest=71222},
+	[46107397] = {quest=70024},
+	[50168163] = {quest=70039},
+	[65727498] = {quest=71223},
+	[64645672] = {quest=71224}, -- was 66195378?
 }, HIGHEST)
