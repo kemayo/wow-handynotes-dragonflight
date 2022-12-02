70b6536c5228730b359c1b14fb208ae9ea3e6e26
Marthammor <marthammor.duin@gmail.com>
2022-12-02 01:22:01 -0500

    Profession Skill Points
    Added locations for one-time use profession skill-ups. Still need quest ids for most to mark them as complete.

---
+++ profession_knowledge/Enchanting.lua
@@ -0,0 +1,77 @@
+local myname, ns = ...
+
+local dfenchknowledge = {
+    label = "Enchanter's Knowledge",
+	note= "This can only be looted once per character.",
+    requires = ns.PROF_DF_ENCHANTING,
+    group = "professionknowledge",
+	minimap = true,
+}
+-- https://www.wowhead.com/guide/professions/knowledge-point-treasure-locations-dragon-isles
+ns.RegisterPoints(ns.WAKINGSHORES, {
+    [57508360] = {
+		note = "Flashfrozen Enclave, in frozen cave system.",
+		loot = {
+			198798, -- Flashfrozen Scroll
+		},
+		quest = nil,
+	},
+    [68002680] = {
+		note = "Scalecracker Keep, next to a lava flower in a flower.",
+		loot = {
+			198675, -- Lava-Infused Seed
+		},
+		quest = nil,
+	},
+    [57505850] = {
+		note = "Use Disenchanted Broom and follow Enchanted Broom, then loot the debris at path's end.",
+		loot = {
+			201012, -- Enchanted Debris
+		},
+		quest = nil,
+	},
+}, dfenchknowledge)
+
+ns.RegisterPoints(ns.OHNAHRANPLAINS, {
+    [61406760] = {
+		note = "Windsong Rise, north of Ohn'iri Springsr.",
+		loot = {
+			198689, -- Stormbound Horn
+		},
+		quest = nil,
+	},
+}, dfenchknowledge)
+
+ns.RegisterPoints(ns.AZURESPAN, {
+    [38505920] = {
+		note = "Azure Archives, in a leveled tomb with a rare mob on the NW side. Tome is lying on the floor to the right of the entrance.",
+		loot = {
+			198799, -- Forgotten Arcane Tome
+		},
+		quest = nil,
+	},
+    [45216114] = {
+		note = "Just east of Azure Archives. Click on Mana-Starved Crystal Cluster to spawn a mob. Kill the mob and click the crystal that spawns.",
+		loot = {
+			201013, -- Faintly Enchanted Remains
+		},
+		quest = nil,
+	},
+    [21004500] = {
+		note = "Inside the mountain.", -- Need info on how to access it!
+		loot = {
+			198694, -- Enriched Earthen Shard
+		},
+		quest = nil,
+	},
+}, dfenchknowledge)
+
+ns.RegisterPoints(ns.THALDRASZUS, {
+    [59907040] = {
+		note = "South of Tyrhold.",
+		loot = {
+			198800, -- Fractured Titanic Sphere
+		},
+		quest = nil,
+	},
+}, dfenchknowledge)
---
+++ profession_knowledge/Inscription.lua
@@ -0,0 +1,80 @@
+local myname, ns = ...
+
+local dfinsknowledge = {
+    label = "Inscriptionist's Knowledge",
+	note= "This can only be looted once per character.",
+    requires = ns.PROF_DF_INSCRIPTION,
+    group = "professionknowledge",
+	minimap = true,
+}
+-- https://www.wowhead.com/guide/professions/knowledge-point-treasure-locations-dragon-isles
+ns.RegisterPoints(ns.WAKINGSHORES, {
+    [67905800] = {
+		note = "Behind a small stone tablebehind a small stone table. Maybe???",
+		loot = {
+			198704, -- Pulsing Earth Rune
+		},
+		quest = nil,
+	},
+}, dfinsknowledge)
+
+ns.RegisterPoints(ns.OHNAHRANPLAINS, {
+    [85702520] = {
+		note = "Timberstep Outpost, hanging on tent entrance.",
+		loot = {
+			198703, -- Sign Language Reference Sheet
+		},
+		quest = nil,
+	},
+}, dfinsknowledge)
+
+ns.RegisterPoints(ns.AZURESPAN, {
+    [46202390] = {
+		note = "Cobalt Assembly, inside a building on an upper level.",
+		loot = {
+			198693, -- Dusty Darkmoon Card
+		},
+		quest = nil,
+	},
+    [43703090] = {
+		note = "Behind an Arcane Commander.",
+		loot = {
+			198686, -- Frosted Parchment
+		},
+		quest = nil,
+	},
+}, dfinsknowledge)
+
+ns.RegisterPoints(ns.THALDRASZUS, {
+    [56304120] = {
+		note = "West of Algeth'ar Academy entrance, on a table near a big telescope. Tome #1",
+		loot = {
+			198659, -- Forgetful Apprentice's Tome #1
+		},
+		quest = nil,
+	},
+    [47244010] = {
+		note = "Above Algeth'era FP, just west in a small building. Interactable {item:380584:Curious Glyph} inside. Click and phase, cross the bridge with some 70 mobs, and kill the neutral mob inside the house. Deliver its dropped item to the glyph to get the item. Tome #2",
+		loot = {
+			198659, -- Forgetful Apprentice's Tome #2
+		},
+		quest = nil,
+	},
+    [56104090] = {
+		note = "Speak to {npc:194856:Siennagosa}. Offer to help set back her Darkmoon Deck. Scattered at her feet are 8 Darkmoon cards. Click them in the correct order (Ace through 8). Speak to her afterward to get the deck.",
+		loot = {
+			201015, -- Counterfit Darkmoon Deck
+		},
+		quest = nil,
+	},
+}, dfinsknowledge)
+
+ns.RegisterPoints(ns.VALDRAKKEN, {
+    [13206368] = {
+		note = "Little Scales Day Care area, brown book lying in the sandbox. Don't ask how it got there.",
+		loot = {
+			198669, -- How to Train Your Whelpling
+		},
+		quest = nil,
+	},
+}, dfinsknowledge)
