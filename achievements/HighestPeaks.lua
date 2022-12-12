c877042576ebf0948ea4c6a2323de646f840f7b0
David Lynch <kemayo@gmail.com>
2022-12-12 00:10:32 -0600

    Highest Peaks: fix the renown level, and redo coords from the vignettes

--- achievements/HighestPeaks.lua
+++ achievements/HighestPeaks.lua
@@ -7,39 +7,39 @@ local HIGHEST = {
 	texture=ns.atlas_texture("racing", {r=0, g=1, b=0}),
 	requires=ns.DRAGONRIDING,
 	hide_before={
-		ns.conditions.MajorFaction(ns.FACTION_DRAGONSCALE, 7),
+		ns.conditions.MajorFaction(ns.FACTION_DRAGONSCALE, 6),
 		ns.conditions.GarrisonTalent(2164),
 	},
 }
 
 ns.RegisterPoints(ns.WAKINGSHORES, {
-	[28714773] = {quest=70826}, -- was 23835308?
-	[43976294] = {quest=70825},
-	[54797412] = {quest=71204},
-	[56534513] = {quest=70823},
-	[73353884] = {quest=70824},
+	[28714773] = {quest=70826, vignette=5395,},
+	[43976295] = {quest=70825, vignette=5394,},
+	[54807412] = {quest=71204, vignette=5410,},
+	[56024541] = {quest=70823, vignette=5392,},
+	[73363885] = {quest=70824, vignette=5393,},
 }, HIGHEST)
 
 ns.RegisterPoints(ns.OHNAHRANPLAINS, {
-	[28317764] = {quest=71200},
-	[30393646] = {quest=71207},
-	[57753080] = {quest=70827},
-	[86313928] = {quest=71208},
+	[28327764] = {quest=71200, vignette=5409,},
+	[30393646] = {quest=71207, vignette=5411,},
+	[57753081] = {quest=70827, vignette=5396,},
+	[86313928] = {quest=71208, vignette=5412,},
 }, HIGHEST)
 
 ns.RegisterPoints(ns.AZURESPAN, {
-	[31912703] = {quest=71215},
-	[37466620] = {quest=71216},
-	[46142498] = {quest=71218},
-	[63084867] = {quest=71220},
-	[74854324] = {quest=71221}, -- was 71986162?
-	[77431837] = {quest=71217},
+	[31912703] = {quest=71215}, -- no vignette?
+	[37476621] = {quest=71216, vignette=5418,},
+	[46142499] = {quest=71218, vignette=5420,},
+	[63084866] = {quest=71220, vignette=5421,},
+	[74854324] = {quest=71221, vignette=5422,},
+	[77441838] = {quest=71217, vignette=5419,},
 }, HIGHEST)
 
 ns.RegisterPoints(ns.THALDRASZUS, {
-	[34048484] = {quest=71222},
-	[46107397] = {quest=70024},
-	[50168163] = {quest=70039},
-	[65727498] = {quest=71223},
-	[64645672] = {quest=71224}, -- was 66195378?
+	[34048484] = {quest=71222, vignette=5423,},
+	[46117398] = {quest=70024, vignette=5390,},
+	[50168163] = {quest=70039, vignette=5391,},
+	[65727498] = {quest=71223, vignette=5424,},
+	[64645672] = {quest=71224, vignette=5425,},
 }, HIGHEST)
