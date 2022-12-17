e7ec2e7ae54692928a5b7b72f53c6ddd2adc2ee9
David Lynch <kemayo@gmail.com>
2022-12-16 19:14:09 -0600

    Azure Span farming of dirt

--- shared/Storms.lua
+++ shared/Storms.lua
@@ -3,6 +3,7 @@
 Stormed Off: 16461
 
 Waking Shores Air: areaPoi 7249
+Azure Span Fire: areaPoi 7238 ~60,67
 
 Iceblade Trio: 69971 + 74009
 Maeleera 193677 + Fieraan 193678 + Leerain 193679
@@ -17,5 +18,10 @@ Pipspark Thundersnap 193684: 69974 + 74022
 +Chargum 193685
 @ Waking Shores 24387558, vignette 5246
 
+Bouldron 193644
+@ Azure Span 57606910, vignette 5225
+
+Zurgaz Corebreaker
+@ Azure Span 60306731, vignette 5244
 
 ]]
